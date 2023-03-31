5ba7857c73bbacc2f0b2269caaf18f6cf131dea7
seokyoungjin <106298467+seokyoungjin@users.noreply.github.com>
2023-03-31 20:56:08 +0900

    add

--- js/slide.js
+++ js/slide.js
@@ -17,16 +17,7 @@ $(function(){
         infinite: true,
         slidesToShow: 2,
         slidesToScroll: 2,
-        // responsive: [
-        //     {
-        //       breakpoint: 768,
-        //       settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1
-        //       }
-        //     }
-        
-        //   ]
+
         responsive: [
             {
                 breakpoint: 1024,
